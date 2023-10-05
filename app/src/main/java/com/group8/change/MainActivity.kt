9967a82fa7515df3b1b91262b542302c8b510026
AdamHlg00 <adamoliver.holgersson@gmail.com>
2023-10-05 13:04:10 +0200

    added topAppBar and some design for the home page, fingers crossed

--- app/src/main/java/com/group8/change/MainActivity.kt
+++ app/src/main/java/com/group8/change/MainActivity.kt
@@ -5,14 +5,26 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.group8.change.ui.design.topAppBar
 import com.group8.change.ui.theme.ChangeTheme
 import java.util.Locale
 
@@ -31,26 +43,110 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    topAppBar {
+                        Structure(
+                            "short",
+                            "loooooooooooooooooooooooooooong",
+                            "adeagagda",
+                            "wow",
+                            "SUUUIII"
+                        )
+                    }
                 }
             }
         }
     }
 }
 
+// Composable for the home screen buttons
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = stringResource(id = R.string.card_title_reflections),
-        modifier = modifier
-    )
+fun CardClickable(text: String, modifier: Modifier = Modifier) {
+    Card(
+        // Defining how the cards look
+        modifier = Modifier
+            .padding(20.dp)
+            .width(130.dp)
+            .height(130.dp),
+        border = BorderStroke(2.dp, Color.Green)
+    ) {
+        Box(
+            // Centers the contents
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                // Text look and text alignment
+                text = text,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.BottomCenter)
+            )
+        }
+    }
+}
+
+// Composable building the home screen
+@Composable
+fun Structure(
+    text1: String,
+    text2: String,
+    text3: String,
+    text4: String,
+    text5: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            Modifier.align(Alignment.Center)
+        ) {
+            Column {
+                CardClickable(
+                    text = text1
+                )
+                CardClickable(
+                    text = text3
+                    )
+            }
+            Column {
+                CardClickable(
+                    text = text2
+                    )
+                CardClickable(
+                    text = text4
+                    )
+            }
+        }
+        Row(
+            Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 100.dp)
+        ) {
+            // Add your elements for the new row here
+            CardClickable(
+                text = text5
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun StructurePreview() {
     ChangeTheme {
-        Greeting("Android")
+        topAppBar {
+            Structure(
+                "short",
+                "loooooooooooooooooooooooooooong",
+                "adeagagda",
+                "wow",
+                "SUUUIII"
+            )
+        }
     }
 }
 
---
+++ app/src/main/java/com/group8/change/ui/design/TopAppBar.kt
@@ -0,0 +1,51 @@
+package com.group8.change.ui.design
+
+import android.annotation.SuppressLint
+import android.widget.Toast
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun topAppBar(content: @Composable () -> Unit) {
+    val context = LocalContext.current
+    // Scaffold required for formatting or something
+    Scaffold(
+        topBar = {
+            // This topbar is center aligned (the text is in the center)
+            CenterAlignedTopAppBar(
+                // The title (text) of the topbar
+                title = {
+                    Text(text = "Yooo")
+
+                },
+                // Here is where you add buttons and stuff
+                actions = {
+                    Button(
+                        onClick = {
+                            Toast.makeText(context, "Hey!", Toast.LENGTH_SHORT).show()
+                        }
+                    ) {
+                        Text(text = "Click")
+                    }
+                },
+                // Here you adjust the colors of the topbar
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        },
+        // Here is where you put the additional contents of the app
+        content = {
+            content()
+        }
+    )
+}
