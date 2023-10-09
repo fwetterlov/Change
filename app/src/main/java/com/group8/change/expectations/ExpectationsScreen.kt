ec32e0466c03887c608db5449d24aeb308ba5537
robin <robin.cobanov@hotmail.com>
2023-10-09 14:28:21 +0200

    Add topbar to Expectations, font changes on QuestionWithTextField

--- app/src/main/java/com/group8/change/expectations/ExpectationsScreen.kt
+++ app/src/main/java/com/group8/change/expectations/ExpectationsScreen.kt
@@ -1,14 +1,16 @@
 package com.group8.change.expectations
 
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.group8.change.components.QuestionWithTextField
 import com.group8.change.expectations.viewmodel.ExpectationsViewModel
 
 
-
 @Composable
 fun ExpectationsScreen(expectationsViewModel: ExpectationsViewModel) {
     QuestionWithTextField(expectationsViewModel)
 }
+
+
 
