afba00e1670491b82595a33c75d1f621f0500abe
fwett <fwetterlov@gmail.com>
2023-10-09 15:27:17 +0200

    flags clickable, calling setLocale

--- app/src/main/java/com/group8/change/MainActivity.kt
+++ app/src/main/java/com/group8/change/MainActivity.kt
@@ -3,16 +3,26 @@ package com.group8.change
 import android.content.Context
 import android.content.res.Configuration
 import android.os.Bundle
+<<<<<<< Updated upstream
+=======
+import android.util.Log
+>>>>>>> Stashed changes
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+<<<<<<< Updated upstream
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+=======
+import androidx.compose.ui.tooling.preview.Preview
+import com.group8.change.api.GetUsers
+import com.group8.change.api.viewmodel.MainViewModel
+import com.group8.change.ui.design.TopAppBar
+>>>>>>> Stashed changes
 import com.group8.change.ui.theme.ChangeTheme
 import java.util.Locale
 
@@ -33,6 +43,12 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("Android")
                 }
+<<<<<<< Updated upstream
+=======
+                //Greeting("Android")
+                //GetUsers(viewModel)
+                //LoginScreen(this)
+>>>>>>> Stashed changes
             }
         }
     }
@@ -55,7 +71,8 @@ fun GreetingPreview() {
 }
 
 
-private fun setLocale(context: Context, languageCode: String) {
+fun setLocale(context: Context, languageCode: String) {
+    Log.d("langCode", languageCode)
     val locale = Locale(languageCode)
     Locale.setDefault(locale)
 
