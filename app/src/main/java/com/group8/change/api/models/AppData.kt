6c7f3de84d7f7cd94f9021b3356946a9adc4491b
Hans Stromquist <frittblas@gmail.com>
2023-10-10 15:59:20 +0200

    can load AppData to nested data class!

--- app/src/main/java/com/group8/change/api/models/AppData.kt
+++ app/src/main/java/com/group8/change/api/models/AppData.kt
@@ -2,30 +2,40 @@ package com.group8.change.api.models
 
 data class AppData(
     val client: Client,
-    val eveningEvaluations: List<Evaluation>,
+    val evening_evaluations: List<Evaluation>,
     val expectations: List<String>,
-    val monthlyEvaluations: List<Evaluation>,
-    val morningEvaluations: List<Evaluation>,
+    val monthly_evaluations: List<Evaluation>,
+    val morning_evaluations: List<Evaluation>,
     val reflections: List<Reflection>,
     val therapist: Therapist
-)
+){
+    constructor() : this(Client("", ""), emptyList(), emptyList(), emptyList(), emptyList(), emptyList(), Therapist(""))
+}
 
 data class Client(
     val role: String,
     val username: String
-)
+){
+    constructor() : this("", "")
+}
 
 data class Evaluation(
     val answers: List<String>,
     val date: String
-)
+){
+    constructor() : this(emptyList(), "")
+}
 
 data class Reflection(
     val data: String,
     val datetime: String,
     val grade: Int
-)
+){
+    constructor() : this("", "", 0)
+}
 
 data class Therapist(
     val username: String
-)
+){
+    constructor() : this("")
+}
