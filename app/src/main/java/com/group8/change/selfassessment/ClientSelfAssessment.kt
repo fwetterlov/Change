0889426089e85286bcb82d4cd657f3df831977aa
fwett <fwetterlov@gmail.com>
2023-10-16 15:53:56 +0200

    Button in main menu added, Package for selfassessment created and setup

---
+++ app/src/main/java/com/group8/change/selfassessment/ClientSelfAssessment.kt
@@ -0,0 +1,29 @@
+package com.group8.change.selfassessment
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ClientSelfAssessment() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Client self assessment"
+        )
+    }
+}
