76ea223740ffd98406bdf5110f7b3ecadbb500d4
AdamHlg00 <adamoliver.holgersson@gmail.com>
2023-10-17 15:06:03 +0200

    Reflections are now properly displayed for therapists

--- app/src/main/java/com/group8/change/reflections/ReflectionScreenTherapist.kt
+++ app/src/main/java/com/group8/change/reflections/ReflectionScreenTherapist.kt
@@ -1,10 +1,13 @@
 package com.group8.change.reflections
 
 import android.graphics.fonts.FontStyle
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,21 +35,60 @@ fun ReflectionScreenTherapist() {
                             .fillMaxSize()
                             .padding(top = 60.dp)
                     ) {
-                        LazyColumn() {
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
                             items(reflectionsSize) {index ->
-                                Text(
-                                    text = "${reflections[index].datetime}",
-                                    fontSize = 30.sp,
-                                    fontWeight = FontWeight.Bold
-                                )
-                                Text(
-                                    text = "${reflections[index].data}",
-                                    fontSize = 20.sp
-                                )
-                                Text(
-                                    text = "Grade: ${reflections[index].grade}",
-                                    fontSize = 20.sp
-                                )
+                                // Splits the date and time into a list
+                                val dateTime = reflections[index].datetime.split("T")
+                                // Splits time into a list of h/m/s
+                                val time = dateTime[1].split(":")
+                                // Splits date into a list of y/m/d
+                                val date = dateTime[0].split("-")
+                                Column (
+                                    modifier = Modifier
+                                        .padding(
+                                            top = 20.dp,
+                                            bottom = 10.dp,
+                                            start = 10.dp,
+                                            end = 10.dp
+                                        )
+                                ) {
+                                    // Big card
+                                    Card (
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                    ) {
+                                        // Card used for inner padding
+                                        Card (
+                                            modifier = Modifier
+                                                .fillMaxSize()
+                                                .padding(
+                                                    top = 10.dp,
+                                                    bottom = 10.dp,
+                                                    start = 10.dp,
+                                                    end = 10.dp
+                                                )
+                                        ){
+                                            // Date
+                                            Text(
+                                                text = "${date[2]}-${date[1]}-${date[0]}",
+                                                fontSize = 30.sp,
+                                                fontWeight = FontWeight.Bold
+                                            )
+                                            // Time
+                                            Text(
+                                                text = "${time[0]}:${time[1]}"
+                                            )
+                                            // Data
+                                            Text(
+                                                text = "${reflections[index].data}",
+                                                fontSize = 20.sp
+                                            )
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
