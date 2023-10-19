b1a9925df1b5e14a97b6674391e5603df652dce0
robin <robin.cobanov@hotmail.com>
2023-10-19 10:45:50 +0200

    Remove unused imports

--- app/src/main/java/com/group8/change/selfassessment/SelfAssessmentScreen.kt
+++ app/src/main/java/com/group8/change/selfassessment/SelfAssessmentScreen.kt
@@ -1,8 +1,6 @@
 package com.group8.change.selfassessment
 
-import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.group8.change.api.models.CurrentUser
 
