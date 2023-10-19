17a8c4f83ec96492a8381260569790d7165ceb00
robin <robin.cobanov@hotmail.com>
2023-10-19 12:39:17 +0200

    EvaluationScreens now use role navigation

---
+++ app/src/main/java/com/group8/change/evaluations/EvaluationScreens.kt
@@ -0,0 +1,36 @@
+package com.group8.change.evaluations
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.group8.change.api.models.CurrentUser
+
+
+@Composable
+fun MonthlyEvaluationScreen(navController: NavController) {
+    if (CurrentUser.data.role == "client") {
+        monthEvaluation(navController)
+    } else if (CurrentUser.data.role == "client addiction") {
+        MonthlyEvaluationAddiction(navController)
+    } else if (CurrentUser.data.role == "therapist") {
+        monthlyEvaluationTherapist(navController)
+    }
+}
+
+@Composable
+fun MorningEvaluationScreen(navController: NavController) {
+    if (CurrentUser.data.role == "client") {
+        morningEvaluation(navController = navController)
+    } else if (CurrentUser.data.role == "therapist") {
+        morningEvaluationTherapist(navController = navController)
+    }
+}
+
+@Composable
+fun EveningEvaluationScreen(navController: NavController) {
+    if (CurrentUser.data.role == "client") {
+        eveningEvaluation(navController = navController)
+    } else if (CurrentUser.data.role == "therapist") {
+        eveningEvaluationTherapist(navController = navController)
+    }
+}
