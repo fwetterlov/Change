d9f5c9b0bcdc10f734d75674acc9734435dfa0d3
robin <robin.cobanov@hotmail.com>
2023-10-19 10:20:37 +0200

    Move and change therapistselfassessmentscreen

--- app/src/main/java/com/group8/change/selfassessment/TherapistSelfAssessment.kt
+++ app/src/main/java/com/group8/change/selfassessment/TherapistSelfAssessment.kt
@@ -1,61 +1,30 @@
 package com.group8.change.selfassessment
 
-import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush.Companion.verticalGradient
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.group8.change.api.models.CurrentAppData
+import androidx.navigation.NavController
+import com.group8.change.R
 import com.group8.change.components.SelfAssessmentGraph
-import com.patrykandpatrick.vico.compose.axis.axisLabelComponent
-import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
-import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
-import com.patrykandpatrick.vico.compose.chart.Chart
-import com.patrykandpatrick.vico.compose.chart.line.lineChart
-import com.patrykandpatrick.vico.compose.chart.line.lineSpec
-import com.patrykandpatrick.vico.compose.chart.scroll.rememberChartScrollState
-import com.patrykandpatrick.vico.compose.component.shape.shader.verticalGradient
-import com.patrykandpatrick.vico.compose.style.ChartStyle
-import com.patrykandpatrick.vico.compose.style.LocalChartStyle
-import com.patrykandpatrick.vico.compose.style.ProvideChartStyle
-import com.patrykandpatrick.vico.core.axis.AxisPosition
-import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
-import com.patrykandpatrick.vico.core.axis.vertical.VerticalAxis
-import com.patrykandpatrick.vico.core.chart.composed.plus
-import com.patrykandpatrick.vico.core.chart.line.LineChart
-import com.patrykandpatrick.vico.core.chart.values.AxisValuesOverrider
-import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
-import com.patrykandpatrick.vico.core.entry.FloatEntry
-
-import com.patrykandpatrick.vico.core.entry.entryModelOf
-import com.patrykandpatrick.vico.core.marker.Marker
-import java.text.SimpleDateFormat
-
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-
-
+import com.group8.change.ui.design.TopAppBar
 
 
 @Composable
-fun TherapistSelfAssessmentScreen() {
-    SelfAssessmentGraph()
+fun TherapistSelfAssessment(navController: NavController) {
+
+    TopAppBar(content = {
+        Column(modifier = Modifier
+            .padding(top = 60.dp)){
+            SelfAssessmentGraph()
+        }
+        },
+        title = stringResource(id = R.string.card_title_self_assessment),
+        navController = navController)
+
 }
 
